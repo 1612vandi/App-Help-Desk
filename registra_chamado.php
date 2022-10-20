711e918b777e861a70f835729a8a2d96327e4bd6
1612vandi <vandidanielmartinsfrancisco@gmail.com>
2022-10-21 00:56:41 +0100

    novos-arquivos-registrar_chamado

---
+++ registra_chamado.php
@@ -0,0 +1,21 @@
+<?php
+
+    //Montando o texto
+    $titulo = str_replace('#','-',$_POST['titulo']);
+    $categoria = str_replace('#','-',$_POST['categoria']);
+    $descricao = str_replace('#','-',$_POST['descricao']);
+    //implode('#',$_POST);
+
+    $texto = $titulo.'#'.$categoria.'#'.$descricao.PHP_EOL;
+    
+    //Abrindo o arquivo
+    $arquivo = fopen('arquivo.hd','a');
+
+    //Escrevendo texto no arquivo
+    fwrite($arquivo,$texto);
+
+    //Fechando o arquivo
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php')
+?>
